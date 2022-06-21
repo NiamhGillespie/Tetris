438f1b387ad3159db4af2bac645ab9ba71f0e433
NiamhGillespie <2549880G@gmail.co.uk>
2022-06-21 15:32:36 +0100

    edited DrawGrid so that grids are correct size

--- Tetris/src/DrawGrid.java
+++ Tetris/src/DrawGrid.java
@@ -6,8 +6,8 @@ public class DrawGrid extends JPanel{
 	
 	@Override
 	public void paintComponent(Graphics graphics) {
-		for ( int x = 0; x <=  Board.numberOfSquares * 20 ; x += 20 )
-		  for ( int y = 0; y <= Board.numberOfSquares * 20; y += 20 ) 
+		for ( int x = 0; x <=  Board.numberOfSquares * 20 - 20 ; x += 20 )
+		  for ( int y = 0; y <= Board.numberOfSquares * 20 - 20; y += 20 ) 
 			 graphics.drawRect(x, y, 20, 20);
 	}
 }
